b9380ab554658be6a5d7860c1ee7e288098b3be2
ladams1776 <ladams1776@yahoo.com>
2020-02-17 13:17:25 -0500

    using redirection in existing script

---
+++ add-newer-local-user.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# This script creates a new user on the local system.
+# You must supply a username as an argument to the script.
+# Optionally, you can also provide a comment for the account as an argument.
+# A password will be auto generated for the account.
+# The username, password, and host for the account will be displayed.
+
+
+# Make sure the script is being executed with the superuser privileges.
+if [[ "${UID}" -ne 0 ]]
+  then
+    echo 'Please run with sudo or as root.' >&2 /dev/null
+    exit 1
+fi
+
+# If they dont supply at least on arg, then give them help.
+if [[ "${#}" -lt 1 ]]
+then 
+  echo "Usage: ${0} USER_NAME [COMMENT]..." >&2 /dev/null
+  exit 1
+fi
+
+# The first parameter is the user name
+USER_NAME="${1}";
+
+# The rest of the parameters are for the account comments.
+shift
+COMMENT="${@}"
+
+# Generate a password
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+
+# Create the user with the password
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Check to see if the useradd command succeeded.
+# We do not want to tell the user that an account was created when it hasnt been.
+if [[ "${?}" -ne 0 ]]
+then 
+  echo 'The account could not be created.' >&2 /dev/null
+  exit 1
+fi
+
+# Set the password.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then 
+  echo 'The password for the account could not be set.' >&2 /dev/null
+  exit 1
+fi
+
+# Display the username, password, and the host where the user was created.
+echo
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
+echo
+echo 'host:'
+echo "${HOSTNAME}"
+exit 0;
--- react-timer-start.sh
+++ react-timer-start.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Start up the react timer
+# We want to start up and run the react timer app, from a local environment, into the docker environment.
+
+# START up the react timer
+# Go into correct directory of where the project lives
 cd ~/react-timer;
-# Clean up previous react timer environment
+# Let's stash and get most recent copy of the repo
+/usr/bin/git stash
+/usr/bin/git pull
+# Clean up previous react timer environment, in the docker-compose.
 docker-compose down;
 docker image rm -f $(docker image ls);
 # Build up new react timer environment
 docker-compose up -d --build;
 
-# Make permissions so I can do whatever I want
+# MAKE permissions so I can do whatever I want
 DIR=$(docker container inspect client | grep "Source" | grep -o "/var/.*");
 PATH=${DIR::-2};
 echo "Path to rsync directory: ${PATH}";
 /bin/chown larry:larry -R $PATH;
 /bin/chmod 777 -R $PATH;
-echo $PATH;
