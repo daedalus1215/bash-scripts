20d89baa09b53bee00fb853140b71a9e15d5f07c
ladams1776 <ladams1776@yahoo.com>
2020-02-17 17:31:44 -0500

    play with the log.sh more

--- log.sh
+++ log.sh
@@ -7,12 +7,33 @@ function log() {
     shift 
     local MESSAGE="${@}"
     echo "${MESSAGE}"
-  fi
+  fi  
   logger -t luser-demo10.sh "${MESSAGE}"
 }
 
-readonly VERBOSE='true'
-log VERBOSE 'YES!'
+
+function backup_file() {
+  # This function created a backup of a file. Returns non-zero status on error.
+  
+  local FILE="${1}"
+
+  # Make sure the file exists.
+  if [[ -f "${FILE}" ]]
+    then
+      local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+      log true "Backing up ${FILE} to ${BACKUP_FILE}."
+
+      # The exit status of the func will be the exit status of the cp command.
+      cp -p ${FILE} ${BACKUP_FILE}
+    else 
+      # The file does not exist, so return a non-zero exit status.
+      return 1
+    fi
+}
+
+
+backup_file '/etc/passwd';
+
 
 
 
